adf2c6803718b9b72b44eeb5c87fea8cfca6510e
h.young <cocotree221@gmail.com>
2023-01-27 12:20:39 +0900

    NavLink 적용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,36 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 /*
-a 태그 href를 이용하면 페이지갱신(mpa)
-Link 컴포넌트를 이용하면 컴포넌트 갱신(spa)
-:a태그로 자동변환이 된다. 
-:to="uri"라는 props가 필요하다.
+NavLink
+:객체를 이용해서 포커스 스타일 적용
+:activeStyle="CSS객체" 이라는 props에 적용
+: to="uri" props 는 필수 
 */
 
 const Header = () => {
+  const active = { color: "yellow" };
   return (
     <header>
       <div className="inner">
         <h1>
-          <Link to="/">logo</Link>
+          <Link to="/">Logo</Link>
         </h1>
         <ul id="gnb">
           <li>
-            <Link to="/department">Department</Link>
+            <NavLink activeStyle={active} to="/department">
+              Department
+            </NavLink>
           </li>
           <li>
-            <Link to="/community">Community</Link>
+            <NavLink to="/community">Community</NavLink>
           </li>
           <li>
-            <Link to="/gallery">Gallery</Link>
+            <NavLink to="/gallery">Gallery</NavLink>
           </li>
           <li>
-            <Link to="/youtube">Youtube</Link>
+            <NavLink to="/youtube">Youtube</NavLink>
           </li>
           <li>
-            <Link to="/lacotion">Location</Link>
+            <NavLink to="/lacotion">Location</NavLink>
           </li>
           <li>
-            <Link to="/join">Join</Link>
+            <NavLink to="/join">Join</NavLink>
           </li>
         </ul>
       </div>
