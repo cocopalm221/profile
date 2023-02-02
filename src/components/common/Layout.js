fb7fb8dd124d9e8256c9b4497b4e933caab2b3f9
h.young <cocotree221@gmail.com>
2023-02-02 10:45:43 +0900

    카카오 지도 1차

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,25 +1,19 @@
 import React, { useEffect, useRef } from "react";
 
 const Layout = (props) => {
-  //useRef는 변수. 즉, state가 아니다.(html 태그 저장하는 용도)
-  //useState()   state는 랜더링 변수/ state가 바뀌면 화면이 바뀐다.
-  // useRef();
-  //순서1.
+
   const frame = useRef(null);
 
   useEffect(() => {
-    // frame useRef를 활용해서 section 태그를 참조해서 css 작업
+
     frame.current.classList.remove("on");
     frame.current.classList.add("on");
     return () => {
-      //아래 구문은 에러가 발생
-      //unmount 가 되면 참조요소가 null이 된다
-      // frame.current.classList.remove("");
+
     };
   });
 
   return (
-    //순서2.상위에 만들어둔 useRef 변수 frame을 ref속성으로 참조
     <section className={`content ${props.title}`} ref={frame}>
       <figure></figure>
       <div className="inner">
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,25 +2,12 @@ import React, { useEffect } from "react";
 import Layout from "../common/Layout";
 
 const Community = () => {
-  //javascript 코드자리
-  //컴포넌트가 마운트, 업데이트(state), 언마운트 시
-  //하고 싶은일을 작성하는 Hook : useEffect
-  //기본형태
-  // useEffect(()=>{
-  //   //하고싶은 일
-  //   //클린업 함수 : 컴포넌트 제거시 실행
-  //   return ()=>{}
-  // },[의존성배열: dependency Array]);
 
   useEffect(() => {
-    console.log("mount:컴포넌트 생성");
-    //axios, html 요소 선택하는 작업
     return () => {
-      console.log("unmount:컴포넌트 제거");
     };
   },[]);
 
-  //jsx 코드는 return() 소괄호
   return <Layout title={"Community"}>Community</Layout>;
 };
 
