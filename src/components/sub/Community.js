1dd4c9b9981fdf892552610645c3c91837f31db0
h.young <cocotree221@gmail.com>
2023-02-10 11:39:50 +0900

    기본기능 완료

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -15,14 +15,17 @@ const Community = () => {
 
   const input = useRef(null);
   const contents = useRef(null);
-  //각 항목을 useState로 사용하면
-  //리랜더링이 많이 일어나므로 ref로 활용한다.
-  //ref로 value를 참조하면 리랜더링을 줄인다.
+  const inputEdit = useRef(null);
+  const textareaEdit = useRef(null);
+
+  //업데이트는 한개만 가능하도록
+  const [Allowed, setAllowed] = useState(true);
+
   const createPost = () => {
     //앞자리 및 뒷자리 공백을 제거하기 위해 trim()사용
     if (input.current.value === "" || contents.current.value === "") {
       resetPost();
-      alert("제목과 본문을 입력하세요.");
+      return alert("제목과 본문을 입력하세요.");
     }
     //새로운 포스트 등록
     //state 업데이트라서 화면 갱신
@@ -32,10 +35,89 @@ const Community = () => {
     ]);
     //입력 저장후 초기화
     resetPost();
+    //연속으로 setState를 진행하면 1번만 된다.
+    //연속으로 setState를 하려면 prev 콜배함수를 활용
+    //업데이트 가능
+    // setAllowed((prev) => {return true});
+    setAllowed((prev) => true);
+    // 모든 목록 닫아주기
+    setPosts((prev) => {
+      const arr = [...prev];
+      const updateArr = arr.map((item, index) => {
+        item.enableUpdate = false;
+        return item;
+      });
+      return updateArr;
+    });
   };
   const resetPost = () => {
     input.current.value = "";
     contents.current.value = "";
+  };
+  //삭제기능
+  const deletePost = (idx) => {
+    if (!window.confirm("정말 삭제하시겠습니까?")) {
+      return;
+    }
+    //idx와 동일한 순서값을 찾아서 state 업데이트
+    //배열.filter는 조건에 맞는
+    //즉, true 배열에 담고, false 면 제외하고
+    //새로운 배열을 돌려준다.
+    setPosts(posts.filter((item, index) => idx !== index));
+  };
+  //업데이트 기능
+  const enableUpdate = (idx) => {
+    // 1개만 업데이트 가능하도록 처리
+    if (!Allowed) return;
+    //토글 시켜준다.
+    setAllowed(false);
+
+    //배열들 중에 idx에 해당하는
+    //1개만 편집이 가능하다고 설정
+    //posts State중에 1개를 설정한다
+    setPosts(
+      posts.map((item, index) => {
+        if (idx === index) {
+          //편집이 가능해요라고 설정하는 키 생성
+          item.enableUpdate = true;
+        }
+        return item;
+      })
+    );
+  };
+  //업데이트취소
+  const disableUpdate = (idx) => {
+    //업데이트 가능하도록 다시 true로 토글
+    setAllowed(true);
+    setPosts(
+      posts.map((item, index) => {
+        if (index === idx) {
+          item.enableUpdate = false;
+        }
+        return item;
+      })
+    );
+  };
+  //내용업데이트
+  const updatePost = (idx) => {
+    //빈문자열 체크
+    if (!inputEdit.current.value.trim() || !textareaEdit.current.value.trim()) {
+      inputEdit.current.value = "";
+      textareaEdit.current.value = "";
+      return alert("수정할 제목과 내용을 입력해주세요.");
+    }
+    setPosts(
+      posts.map((item, index) => {
+        if (idx === index) {
+          item.title = inputEdit.current.value;
+          item.content = textareaEdit.current.value;
+          item.enableUpdate = false;
+        }
+        return item;
+      })
+    );
+    //업데이트 가능하도록
+    setAllowed(true);
   };
   //디버깅
   useEffect(() => {
@@ -76,14 +158,49 @@ const Community = () => {
             //그러나 기본은 가능하면 본인이 key를 관리
             return (
               <article key={index}>
-                <div className="txt">
-                  <h2>{item.title}</h2>
-                  <p>{item.content}</p>
-                </div>
-                <div className="btnSet">
-                  <button>EDIT</button>
-                  <button>DELETE</button>
-                </div>
+                {item.enableUpdate ? (
+                  // 업데이트일때 보여줄 jsx
+                  <>
+                    <div className="txt">
+                      <input
+                        type="text"
+                        defaultValue={item.title}
+                        placeholder="제목을 입력하세요."
+                        ref={inputEdit}
+                      />
+                      <br />
+                      <textarea
+                        cols="30"
+                        rows="5"
+                        defaultValue={item.content}
+                        placeholder="내용을 입력하세요."
+                        ref={textareaEdit}
+                      ></textarea>
+                    </div>
+                    <div className="btnSet">
+                      {/* 업데이트취소 */}
+                      <button onClick={() => disableUpdate(index)}>
+                        CANCEL
+                      </button>
+                      {/* 내용 업데이트 */}
+                      <button onClick={() => updatePost(index)}>SAVE</button>
+                    </div>
+                  </>
+                ) : (
+                  // 목록일때 보여줄 jsx
+                  <>
+                    <div className="txt">
+                      <h2>{item.title}</h2>
+                      <p>{item.content}</p>
+                    </div>
+                    <div className="btnSet">
+                      {/* 업데이트기능 */}
+                      <button onClick={() => enableUpdate(index)}>EDIT</button>
+                      {/* 삭제기능 */}
+                      <button onClick={() => deletePost(index)}>DELETE</button>
+                    </div>
+                  </>
+                )}
               </article>
             );
           })
