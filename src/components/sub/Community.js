cdb5cab87f63bcca90d973179585cd8e1d80ff0d
h.young <cocotree221@gmail.com>
2023-02-10 13:02:55 +0900

    useForm, yup 적용

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,9 +1,38 @@
 import React, { useEffect, useRef, useState } from "react";
+//https://react-hook-form.com/
+//npm install react-hook-form
+//https://github.com/jquense/yup/tree/pre-v1
+//npm install -S yup
+//npm install @hookform/resolvers
 import Layout from "../common/Layout";
 import CommunityCard from "./CommunityCard";
 
+//01.useForm import
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+//02. form 요소의 항목별 에러 체크
+const schema = yup.object({
+  title: yup.string().trim().required("제목을 입력해주세요."),
+  content: yup.string().trim().required("내용을 입력해주세요."),
+});
+
 const Community = () => {
-  // 데모용 데이터 생성
+  //03. useForm 생성
+  //register 각각의 form 의 name을 설정
+  //handleSubmit : form 에서 onSubmit 할때 실행됨
+  //reset : form에서 reset 할때 실행
+  //formState:{errors} : yup 에러 출력 활용
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema), //yup과 연결시켜줌.
+    mode: "onChange", //mode가 onChange면 실행
+  });
   const initPost = [
     { title: "Hello 1", content: "Welocme To React!" },
     { title: "Hello 2", content: "Welocme To React!" },
@@ -11,37 +40,21 @@ const Community = () => {
     { title: "Hello 4", content: "Welocme To React!" },
     { title: "Hello 5", content: "Welocme To React!" },
   ];
-  //출력 목록 관리 state
+
   const [posts, setPosts] = useState(initPost);
 
-  const input = useRef(null);
-  const contents = useRef(null);
   const inputEdit = useRef(null);
   const textareaEdit = useRef(null);
-
-  //업데이트는 한개만 가능하도록
   const [Allowed, setAllowed] = useState(true);
 
-  const createPost = () => {
-    //앞자리 및 뒷자리 공백을 제거하기 위해 trim()사용
-    if (input.current.value === "" || contents.current.value === "") {
-      resetPost();
-      return alert("제목과 본문을 입력하세요.");
-    }
-    //새로운 포스트 등록
-    //state 업데이트라서 화면 갱신
-    setPosts([
-      ...posts,
-      { title: input.current.value, content: contents.current.value },
-    ]);
-    //입력 저장후 초기화
-    resetPost();
-    //연속으로 setState를 진행하면 1번만 된다.
-    //연속으로 setState를 하려면 prev 콜배함수를 활용
-    //업데이트 가능
-    // setAllowed((prev) => {return true});
+  const createPost = (data) => {
+    // data======> {title:title, content:content}
+    setPosts([...posts, data]);
+    //...register("title")
+    //...register("content")
+    reset(); //register를 비움
+
     setAllowed((prev) => true);
-    // 모든 목록 닫아주기
     setPosts((prev) => {
       const arr = [...prev];
       const updateArr = arr.map((item, index) => {
@@ -51,35 +64,21 @@ const Community = () => {
       return updateArr;
     });
   };
-  const resetPost = () => {
-    input.current.value = "";
-    contents.current.value = "";
-  };
+
   //삭제기능
   const deletePost = (idx) => {
     if (!window.confirm("정말 삭제하시겠습니까?")) {
       return;
     }
-    //idx와 동일한 순서값을 찾아서 state 업데이트
-    //배열.filter는 조건에 맞는
-    //즉, true 배열에 담고, false 면 제외하고
-    //새로운 배열을 돌려준다.
     setPosts(posts.filter((item, index) => idx !== index));
   };
   //업데이트 기능
   const enableUpdate = (idx) => {
-    // 1개만 업데이트 가능하도록 처리
     if (!Allowed) return;
-    //토글 시켜준다.
     setAllowed(false);
-
-    //배열들 중에 idx에 해당하는
-    //1개만 편집이 가능하다고 설정
-    //posts State중에 1개를 설정한다
     setPosts(
       posts.map((item, index) => {
         if (idx === index) {
-          //편집이 가능해요라고 설정하는 키 생성
           item.enableUpdate = true;
         }
         return item;
@@ -88,7 +87,6 @@ const Community = () => {
   };
   //업데이트취소
   const disableUpdate = (idx) => {
-    //업데이트 가능하도록 다시 true로 토글
     setAllowed(true);
     setPosts(
       posts.map((item, index) => {
@@ -101,7 +99,6 @@ const Community = () => {
   };
   //내용업데이트
   const updatePost = (idx) => {
-    //빈문자열 체크
     if (!inputEdit.current.value.trim() || !textareaEdit.current.value.trim()) {
       inputEdit.current.value = "";
       textareaEdit.current.value = "";
@@ -117,9 +114,9 @@ const Community = () => {
         return item;
       })
     );
-    //업데이트 가능하도록
     setAllowed(true);
   };
+
   //디버깅
   useEffect(() => {
     console.log(posts);
@@ -128,23 +125,25 @@ const Community = () => {
     <Layout title={"Community"}>
       {/* 입력폼 */}
       <div className="inputBox">
-        <form>
-          <input type="text" placeholder="제목을 입력하세요" ref={input} />
+        <form onSubmit={handleSubmit(createPost)}>
+          <input
+            type="text"
+            placeholder="제목을 입력하세요"
+            {...register("title")}
+          />
+          <span className="err">{errors.title?.message}</span>
           <br />
           <textarea
             clols="30"
             rows="5"
             placeholder="본문을 입력하세요"
-            ref={contents}
+            {...register("content")}
           ></textarea>
+          <span className="err">{errors.content?.message}</span>
           <div className="btnSet">
             {/* form 안쪽에 버튼은 type을 정의한다. type="button" */}
-            <button type="button" onClick={resetPost}>
-              CAMCEL
-            </button>
-            <button type="button" onClick={createPost}>
-              WRIGHT
-            </button>
+            <button type="reset">CANCEL</button>
+            <button type="submit">WRITE</button>
           </div>
         </form>
       </div>
@@ -154,9 +153,6 @@ const Community = () => {
         {
           // posts.map((item,index)=>(jsx));
           posts.map((item, index) => {
-            //uuid:https://www.npmjs.com/package/uuid
-            //중복되지않는 key를 만들어주는 라이브러리
-            //그러나 기본은 가능하면 본인이 key를 관리
             return (
               <CommunityCard
                 key={index}
